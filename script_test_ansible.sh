2f85dd91d01a2fa8aab3807211c92b4dd3bb8289
huynd86 <huynd86@fpt.com.vn>
2023-08-31 17:33:27 +0700

    [+] add vm with vagrant

--- script_test_ansible.sh
+++ script_test_ansible.sh
@@ -2,4 +2,4 @@
 source ansible-env/bin/activate
 pip install -r requirements.txt
 ansible-playbook -i inventory/inventory_create_ansible_user.yml playbook/create_ansible_user.yml
-ansible-playbook -i inventory/inventory.yaml playbook/deploy_mock_api_playbook.yml
+ansible-playbook -i inventory/inventory.yaml playbook/test-playbook.yaml
